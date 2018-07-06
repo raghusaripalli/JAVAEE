188021950a73dbb0abfc00bc821acfc15dcbbb4a
raghusaripalli <rvs1604@gmail.com>
2018-07-06 14:50:46 +0530

    06-JUL

--- src/rest/JobRest.java
+++ src/rest/JobRest.java
@@ -1,16 +1,25 @@
 package rest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.sql.rowset.CachedRowSet;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 
 import oracle.jdbc.rowset.OracleCachedRowSet;
 
@@ -62,6 +71,56 @@ public class JobRest {
 			}
 			else {
 				crs.close();
+				throw new NotFoundException();
+			}
+		}
+		catch(SQLException e) {
+			throw new InternalServerErrorException();
+		}
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void addJob(@FormParam("id") String id, @FormParam("title") String title) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			PreparedStatement ps = con.prepareStatement("insert into jobs (job_id, job_title) values (?,?)");
+			ps.setString(1, id);
+			ps.setString(2, title);
+			ps.executeUpdate();
+		}
+		catch(Exception e) {
+			throw new InternalServerErrorException();
+		}
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void updateJob(@PathParam("id") String id, @FormParam("title") String title) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			PreparedStatement ps = con.prepareStatement("update jobs set job_title=? where job_id=?");
+			ps.setString(1, title);
+			ps.setString(2, id);
+			ps.executeUpdate();
+		}
+		catch(Exception e) {
+			throw new InternalServerErrorException();
+		}
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public void deleteJob(@PathParam("id") String id) throws ClassNotFoundException {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			PreparedStatement ps = con.prepareStatement("delete from jobs where job_id=?");
+			ps.setString(1, id);
+			if(ps.executeUpdate()!=1) {
 				throw new NotFoundException();
 			}
 		}
